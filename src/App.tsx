317c5cc19de52dc8e02494b4236eecd2e6456e72
ktolleshaug <ktolleshaug@gmail.com>
2024-04-11 00:13:02 -0500

    init new design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`bg-no-repeat bg-cover bg-center h-screen w-screen bg-[linear-gradient(60deg,rgba(255,265,150,0.5),rgba(0,228,255,0.5)),url('../public/images/bg.jpg')]`}>
+        <div className='flex flex-col absolute w-full h-full justify-center items-center'>
+          <div className='flex flex-col bg-white w-full md:w-1/2 lg:w-1/4 rounded-3xl py-24'>
+            <div className='flex flex-row items-center justify-center pb-8' >
+              <hr className='flex w-1/2'></hr>
+              <div className=' flex px-6'>
+                <img className='rounded-full' src={'/images/profile.jpg'} alt='profileimage' aria-label="Profile Image"></img>
+              </div>
+              <hr className='flex w-1/2'></hr>
+            </div>
+            <div className='flex flex-col items-center font-sans'>
+              <div className='flex text-5xl '>
+                <p> Kirk Tolleshaug</p>
+              </div>
+              <div className='flex flex-col items-center text-2xl py-8'>
+                <p>Senior Software Engineer</p>
+                <p>Full Stack</p>
+                <p>Angular/React - .Net - SQL</p>
+              </div>
+            </div>
+            <hr className='py-2'></hr>
+            <div className='flex flex-row justify-center h-24 space-x-2 text-gray-800'>
+              <a className='flex h-full hover:text-[rgb(0,119,181)]' href='https://www.linkedin.com/in/ktolleshaug/' target='_blank' rel='noreferrer' aria-label='Linkedin'><FontAwesomeIcon className='flex h-full' icon={faLinkedin}></FontAwesomeIcon></a>
+              <a className='flex h-full hover:text-green-600' href='https://github.com/ktoll2' target='_blank' rel='noreferrer' aria-label='Github'><FontAwesomeIcon className='flex h-full' icon={faGithub} ></FontAwesomeIcon></a>
+              <a className='flex h-full hover:text-yellow-500' href='https://observatory.mozilla.org/analyze/ktoll.dev' target='_blank' rel='noreferrer' aria-label='Mozilla Observatory'><FontAwesomeIcon className='flex h-full' icon={faShieldHalved}></FontAwesomeIcon></a>
+            </div>
+          </div>
+          <p>&#169; Kirk Tolleshaug {new Date().getFullYear()}</p>
+        </div>
+      </div>
+
     </div>
   );
 }
